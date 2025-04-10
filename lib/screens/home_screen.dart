77126d66709cf6558333eb540b55146f979725ba
AdamG1t-hub <222637@student.upm.edu.my>
2025-04-10 20:42:47 +0800

    Lab 3 Unti 3.03.00

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_lab3/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       //Home Screen Listview
       body: ListView(
         children: [
-
           // Container around 3TC
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -20,50 +18,56 @@ class HomeScreen extends StatelessWidget {
             // 1/3rd Top Column (TC)
             child: Column(
               children: [
-            
                 //TC-Row 1
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween, //Horizonral Alignment in Row Widget
+                  mainAxisAlignment:
+                      MainAxisAlignment
+                          .spaceBetween, //Horizonral Alignment in Row Widget
                   children: [
-            
                     //Row 1 - Left Partition
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Good morning", style: TextStyle(
-                          fontSize: 17, fontWeight: FontWeight.w500,
-                        )),
+                        Text(
+                          "Good morning",
+                          style: AppStyles.headLineStyle3,
+                        ),
                         SizedBox(height: 5),
-                        Text("Book Tickets", style: TextStyle(
-                          fontSize: 26, fontWeight: FontWeight.w500, color: Color(0xFF3b3b3b) 
-                        )),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headLineStyle1,
+                        ),
                       ],
                     ),
-            
+
                     //Row 1 - Right Partition
                     Container(
-                      color: Colors.red,
-                      width: 100,
-                      height: 70,
-                    )
+                       
+                      width: 50, 
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/logo.png")
+
+                         )
+                        )
+                      ),
                   ],
                 ),
-            
+
                 //TC-Row 2
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(" Search icon "),
-                    Text(" Empty space"),
-                  ],
-                )
+                  children: [Text(" Search icon "), Text(" Empty space")],
+                ),
               ],
             ),
-          )
+          ),
 
           //ADD MORE LISTVIEW CHILDREN
         ],
-      )
+      ),
     );
   }
 }
